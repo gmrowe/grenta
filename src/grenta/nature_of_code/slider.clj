480b4a6b4ecf1cb9eda5e31390ec7635dcfa5fad
gmrowe <garrett.m.rowe@gmail.com>
2024-11-27 22:28:37 -0700

    Implement a slider widget for animations

--- src/grenta/nature_of_code/slider.clj
+++ src/grenta/nature_of_code/slider.clj
@@ -2,7 +2,6 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]
             [clojure.pprint :as pp]))
-
 (defn clamp
   [n lo hi]
   (cond
@@ -11,67 +10,41 @@
     :else n))
 
 (defn new-slider
-  [x y width height]
-  {:slider-length width
-   :slider-height height
-   :slider-inactive-color (q/color 190)
-   :slider-active-color (q/color 50)
-   :slider-outline (q/color 10)
-   :knob-diameter 15
-   :knob-color 10
-   :slider-x (/ (- (q/width) width) 2.0)
-   :slider-y (/ (- (q/height) height) 2.0)
-   :slider-corner-radius 4
-   :knob-x (/ (- (q/width) width) 2.0)
-   :knob-y (+ (/ height 2.0) (/ (- (q/height) height) 2.0))})
-
-(defn setup
-  []
-  (let [length 180
-        height 5]
-    (new-slider (/ (- (q/width) length) 2.0)
-                (/ (- (q/height) height) 2.0)
-                length
-                height)))
-
-(defn update-state
-  [state]
-  state)
+  [x y width height & {:as opts}]
+  (merge {:slider-length width
+          :slider-height height
+          :slider-inactive-color (q/color 190)
+          :slider-active-color (q/color 50)
+          :slider-outline (q/color 10)
+          :knob-diameter 15
+          :knob-color 10
+          :slider-x x
+          :slider-y y
+          :slider-corner-radius 4
+          :knob-x x
+          :knob-y (+ y (/ height 2.0))
+          :value 0.0}
+         opts))
 
 (defn draw
+  [{:keys
+      [slider-length slider-height slider-inactive-color slider-active-color
+       slider-outline slider-corner-radius knob-diameter knob-color slider-x slider-y
+       knob-x knob-y]}]
+  ;; Draw track
+  (q/fill slider-inactive-color)
+  (q/stroke slider-outline)
+  (q/rect slider-x slider-y slider-length slider-height slider-corner-radius)
+  (q/fill slider-active-color)
+  (q/rect slider-x slider-y (- knob-x slider-x) slider-height slider-corner-radius)
+  ;; Draw knob
+  (q/fill knob-color)
+  (q/ellipse knob-x knob-y knob-diameter knob-diameter))
+
+(defn update-slider-value
   [state]
-  ;; Draw horizontal rectangle
-  (q/background 250)
-  (let [{:keys
-           [slider-length
-            slider-height
-            slider-inactive-color
-            slider-active-color
-            slider-outline
-            slider-corner-radius
-            knob-diameter
-            knob-color
-            slider-x
-            slider-y
-            knob-x
-            knob-y]}
-          state]
-    (q/fill slider-inactive-color)
-    (q/stroke slider-outline)
-    (q/rect slider-x slider-y slider-length slider-height slider-corner-radius)
-    (q/fill slider-active-color)
-    (q/rect slider-x slider-y (- knob-x slider-x) slider-height slider-corner-radius)
-    ;; Draw circular knob
-    (q/fill knob-color)
-    (q/ellipse knob-x knob-y knob-diameter knob-diameter)
-
-    ;; Make knob slidable across slider
-    ;; -- done in mouse-dragged functon
-
-    ;; Output slider state
-    (q/text (format "%.0f%%" (* 100 (/ (- knob-x slider-x) slider-length)))
-            slider-x
-            (- slider-y 20))))
+  (let [{:keys [knob-x slider-x slider-length]} state]
+    (assoc state :value (/ (- knob-x slider-x) slider-length))))
 
 (defn slider-clicked?
   [state click-event]
@@ -85,7 +58,9 @@
 (defn mouse-clicked
   [state click-event]
   (if (slider-clicked? state click-event)
-    (assoc state :knob-x (:x click-event))
+    (-> state
+        (assoc :knob-x (:x click-event))
+        update-slider-value)
     state))
 
 (defn knob-dragged?
@@ -104,19 +79,35 @@
     (nil? (:dragging-knob? state))
       (assoc state :dragging-knob? (knob-dragged? state drag-event))
     (:dragging-knob? state)
-      (assoc state :knob-x (clamp x slider-x (+ slider-x slider-length)))
+      (-> state
+          (assoc :knob-x (clamp x slider-x (+ slider-x slider-length)))
+          update-slider-value)
     :else
       state))
 
-(q/defsketch slider
-  :size [300 200]
-  :title "Slider"
-  :settings #(q/smooth 2)
-  :features [:no-safe-fns]
-  :setup setup
-  :update update-state
-  :draw draw
-  :mouse-clicked mouse-clicked
-  :mouse-dragged mouse-dragged
-  :mouse-released (fn [state _] (dissoc state :dragging-knob?))
-  :middleware [m/fun-mode])
+(defn mouse-released
+  [state _event]
+  (dissoc state :dragging-knob?))
+
+(comment
+  (defn setup
+    []
+    (let [length 180
+          height 5]
+      (new-slider (/ (- (q/width) length) 2.0)
+                  (/ (- (q/height) height) 2.0)
+                  length
+                  height)))
+
+  (q/defsketch slider
+    :size [300 200]
+    :title "Slider"
+    :settings #(q/smooth 2)
+    :features [:no-safe-fns]
+    :setup setup
+    :update update-state
+    :draw draw
+    :mouse-clicked mouse-clicked
+    :mouse-dragged mouse-dragged
+    :mouse-released (fn [state _] (dissoc state :dragging-knob?))
+    :middleware [m/fun-mode]))
